321d48252f065e3897ab70f09fd4ffcd073fff07
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-04-18 18:22:46 +0500

    db models start

---
+++ tgbot/models/models.py
@@ -0,0 +1,77 @@
+from gino import Gino
+from sqlalchemy import (
+    Column, Integer, String,
+    DateTime, ForeignKey, BigInteger,
+    Boolean
+)
+
+db = Gino()
+
+
+class News(db.Model):
+
+    __tablename__ = 'news'
+
+    Id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    title = Column(String)
+    created_date = Column(DateTime)
+    photo_path = Column(String(150))
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'{self.__tablename__}: {self.Id}'
+
+
+class Category(db.Model):
+
+    __tablename__ = 'category'
+
+    Id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'{self.__tablename__}: {self.Id}'
+
+
+class InnerCategory(db.Model):
+
+    __tablename__ = 'inner_category'
+
+    Id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    category = Column(Integer, ForeignKey('category.Id'))
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'{self.__tablename__}: {self.Id}'
+
+
+class Product(db.Model):
+
+    __tablename__ = 'product'
+
+    Id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    price = Column(BigInteger)
+    photo_path = Column(String(150))
+    title = Column(String)
+    in_stock = Column(Boolean, default=True)
+    quantity = Column(Integer)
+    category = Column(
+        Integer,
+        ForeignKey('category.Id')
+    )
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f'{self.__tablename__}: {self.Id}'
