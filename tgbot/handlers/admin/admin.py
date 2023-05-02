1455ab8eebd5511fe44ecb1671c4aacf73fd5037
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2023-05-02 18:35:20 +0500

    hotfix/admin_filter

--- tgbot/handlers/admin/admin.py
+++ tgbot/handlers/admin/admin.py
@@ -1,10 +1,15 @@
 from aiogram import Dispatcher
+from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 
 
-async def admin_start(message: Message):
+async def admin_start(message: Message, state: FSMContext):
+    await state.finish()
     await message.reply("Hello, admin!")
 
 
 def register_admin(dp: Dispatcher):
-    dp.register_message_handler(admin_start, commands=["start"], state="*", is_admin=True)
+    dp.register_message_handler(
+        admin_start, commands=["start"],
+        state="*", is_superuser=True
+    )
